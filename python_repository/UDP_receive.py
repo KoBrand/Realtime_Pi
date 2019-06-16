63afc1ab4bfdbe8e85603c18ac701528e1b0319e
KoBrand <konstantin.brand@googelmail.com>
2019-06-16 16:59:53 +0200

    update ip

--- python_repository/UDP_receive.py
+++ python_repository/UDP_receive.py
@@ -1,7 +1,7 @@
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+UDP_IP = "192.168.178.22"
+UDP_PORT = 5555
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
